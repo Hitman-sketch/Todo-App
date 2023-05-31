4ed8b764fa4ffc231be1fc686990f21ae4431882
Michael Attoh <52075079+Hitman-sketch@users.noreply.github.com>
2023-05-31 20:23:37 +0000

    working on the modile view and would have to modify some loose ends in the labels section.

--- index.js
+++ index.js
@@ -67,12 +67,11 @@ function toggle_icon() {
 
 
 //---------------------------------------------------------------------------------------------
-
 // Function to toggle the list item when the circle icon is clicked
 function toggleListItem() {
-   // Toggle the 'icon-check' class on the circle icon
+  // Toggle the 'icon-check' class on the circle icon
   this.classList.toggle('icon-check');
-   // Find the corresponding list item and todo text
+  // Find the corresponding list item and todo text
   let listItem = this.parentNode;
   let todoText = listItem.querySelector('p');
   // Toggle the strikethrough effect on the todo text
@@ -94,10 +93,12 @@ function toggleListItem() {
     // Click event listener to the cross icon removes the item from the list when the cross icon is clicked
     crossIcon.addEventListener('click', function () {
       listItem.remove();
+      updateItemsLeft();
     });
   }
-}
 
+  updateItemsLeft();
+}
 
 // Function to handle adding a new list item
 function addListItem() {
@@ -109,7 +110,13 @@ function addListItem() {
 
     // Make the list item draggable
     listItem.draggable = true;
-     // Create circle icon and append it to the list item
+
+    // Create todo text element and append it to the list item
+    let todoText = document.createElement('p');
+    todoText.textContent = text;
+    listItem.appendChild(todoText);
+
+    // Create circle icon and append it to the list item
     let circleSpan = document.createElement('span');
     circleSpan.classList.add('icon-circle');
     circleSpan.classList.add('align');
@@ -118,11 +125,6 @@ function addListItem() {
     circleIcon.setAttribute('alt', 'circle');
     circleSpan.appendChild(circleIcon);
     listItem.appendChild(circleSpan);
-
-    // Create todo text element and append it to the list item
-    let todoText = document.createElement('p');
-    todoText.textContent = text;
-    listItem.appendChild(todoText);
 
     // Check if there is a first item in the list
     let firstItem = todoList.firstChild;
@@ -141,7 +143,6 @@ function addListItem() {
       this.classList.remove('hover');
     });
 
-    
     // Add drag and drop event listeners to the list item
     listItem.addEventListener('dragstart', handleDragStart);
     listItem.addEventListener('dragenter', handleDragEnter);
@@ -153,11 +154,13 @@ function addListItem() {
     // Append the new list item to the todoList
     todoList.insertBefore(listItem, todoList.firstChild);
 
-
     // Clear the input text
     inputText.value = '';
+
+    updateItemsLeft();
   }
 }
+
 // Variables to store references to the dragged item and the current drop target
 let draggedItem = null;
 let dropTarget = null;
@@ -168,7 +171,7 @@ function handleDragStart(event) {
   this.classList.add('dragging');
   // Set the drag effect to 'move'
   event.dataTransfer.effectAllowed = 'move';
-  //Type of data to drag and drop
+  // Type of data to drag and drop
   event.dataTransfer.setData('text/plain', '');
 }
 
@@ -183,7 +186,7 @@ function handleDragEnter(event) {
 // Function to handle the dragover event
 function handleDragOver(event) {
   event.preventDefault();
-   // Set the effect to 'move' for the dragover event
+  // Set the effect to 'move' for the dragover event
   event.dataTransfer.dropEffect = 'move';
 }
 
@@ -210,6 +213,8 @@ function handleDrop(event) {
     this.classList.remove('dragover');
     draggedItem = null;
     dropTarget = null;
+
+    updateItemsLeft();
   }
 }
 
@@ -220,7 +225,133 @@ function handleDragEnd() {
   listItems.forEach((item) => item.classList.remove('dragover'));
 }
 
+// Function to handle the click event on the circle icon
+function handleCircleIconClick(event) {
+  toggleListItem.call(event.target);
+}
+
+// Function to handle the click event on the check icon
+function handleCheckIconClick(event) {
+  toggleListItem.call(event.target.parentNode);
+}
+
 document.addEventListener('DOMContentLoaded', function () {
   let circleIcon = document.getElementById('circleIcon');
   circleIcon.addEventListener('click', addListItem);
+
+  let todoList = document.getElementById('todoList');
+  todoList.addEventListener('click', function (event) {
+    if (event.target.tagName === 'SPAN') {
+      toggleListItem.call(event.target);
+    }
+  });
+
+  let allLabel = document.querySelector('.cont_All');
+  allLabel.addEventListener('click', showAllTodos);
+
+  let completedLabel = document.querySelector('.cont_completed');
+  completedLabel.addEventListener('click', showCompletedTodos);
+
+  let activeLabel = document.querySelector('.cont_active');
+  activeLabel.addEventListener('click', showActiveTodos);
+
+  let clearCompletedLabel = document.querySelector('.clear');
+  clearCompletedLabel.addEventListener('click', clearCompletedTodos);
+
+  // Function to toggle the list item when the circle icon is clicked
+  function toggleListItem() {
+    // Toggle the 'icon-check' class on the circle icon
+    this.classList.toggle('icon-check');
+    // Find the corresponding list item and todo text
+    let listItem = this.parentNode;
+    let todoText = listItem.querySelector('p');
+    // Toggle the strikethrough effect on the todo text
+    todoText.classList.toggle('strikethrough');
+
+    updateItemsLeft();
+  }
+
+  // Function to handle the click event on the circle icon
+  function handleCircleIconClick(event) {
+    toggleListItem.call(event.target);
+  }
+
+  // Function to handle the click event on the check icon
+  function handleCheckIconClick(event) {
+    toggleListItem.call(event.target.parentNode);
+  }
+
+  // Add click event listeners to the circle icons in all sections
+  let circleIcons = document.querySelectorAll('.icon-circle');
+  circleIcons.forEach((circleIcon) => {
+    circleIcon.addEventListener('click', handleCircleIconClick);
+  });
+
+  // Add click event listeners to the check icons in all sections
+  let checkIcons = document.querySelectorAll('.icon-check');
+  checkIcons.forEach((checkIcon) => {
+    checkIcon.addEventListener('click', handleCheckIconClick);
+  });
+
+
+  function showAllTodos() {
+    let todos = document.querySelectorAll('#todoList li');
+    todos.forEach((todo) => {
+      todo.style.display = 'block';
+    });
+  }
+
+  function showActiveTodos() {
+    let todos = document.querySelectorAll('#todoList li');
+    todos.forEach((todo) => {
+      let circleIcon = todo.querySelector('.icon-circle');
+      let todoText = todo.querySelector('p');
+      if (!circleIcon.classList.contains('icon-check')) {
+        todoText.style.textDecoration = 'none';
+        todo.style.display = 'block';
+      } else {
+        todo.style.display = 'none';
+      }
+    });
+  }
+
+  function showCompletedTodos() {
+    let todos = document.querySelectorAll('#todoList li');
+    todos.forEach((todo) => {
+      let circleIcon = todo.querySelector('.icon-circle');
+      let todoText = todo.querySelector('p');
+      if (circleIcon.classList.contains('icon-check')) {
+        todoText.style.textDecoration = 'line-through';
+        todo.style.display = 'block';
+      } else {
+        todoText.style.textDecoration = 'none';
+        todo.style.display = 'none';
+      }
+    });
+  }
+
+  function clearCompletedTodos() {
+    let completedTodos = document.querySelectorAll('#todoList li .icon-check');
+    completedTodos.forEach((completedTodo) => {
+      let listItem = completedTodo.parentNode;
+      let todoText = listItem.querySelector('p');
+      if (todoText.classList.contains('strikethrough')) {
+        listItem.remove();
+      }
+    });
+
+    updateItemsLeft();
+  }
+
+  // Initial update of items left count
+  updateItemsLeft();
 });
+
+// Function to update the items left count
+function updateItemsLeft() {
+  let itemsList = document.querySelectorAll('#todoList li');
+  let removedItems = document.querySelectorAll('#todoList li.removed');
+  let itemsLeft = itemsList.length - removedItems.length;
+  let itemsLeftLabel = document.querySelector('.items-left');
+  itemsLeftLabel.textContent = `${itemsLeft} ${itemsLeft === 1 ? 'item' : 'items'} left`;
+}
